bb7c2123b0957bbae13e5c531453107f88665f57
Fahad-Hafeez <79376608+Fahad-Hafeez@users.noreply.github.com>
2021-07-02 20:21:15 +0100

    working with numbers

---
+++ WorkingWithNumbers/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    # %f symbol stands for decimal number not whole number meaning it'll goe you answer in decimals.
+    printf("%f", 8.9);
+    printf("%f", 5.0 + 4.5);
+    printf("%f", 5.0 - 4.5);
+    printf("%f", 5.0 * 4.5);
+    printf("%f", 5.0 / 4.5);
+
+
+    return 0;
+}
+
+int main()
+(
+
+    # %d symbol stands for variable, meaning you'll be able to print numbers which don't have decimals and numbers after it.
+    print("%d", pow(2, 3) );
+    print("%d", sqrt(36) );
+    print("%d", ciel(40.9864) );
+    print("%d", floor(36.656) );
+
+
+)
