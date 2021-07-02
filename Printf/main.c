c27e6c12e6e146e03be82484cf781e77b82f3538
Fahad-Hafeez <79376608+Fahad-Hafeez@users.noreply.github.com>
2021-07-02 20:04:46 +0100

    printf files
    these files show you how to call data types or variables inside a print statement

---
+++ Printf/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int favNum = 90;
+    char myChar = 'i';
+    printf("My favourite %s is %d", "number", favNum);
+    printf("My favourite %c is %f", myChar, favNum);
+
+}
